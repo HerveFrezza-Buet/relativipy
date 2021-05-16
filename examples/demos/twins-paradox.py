bf4085c1571aa682d02706dcd0bec667dd908d53
Hervé Frezza-Buet <herve.frezza-buet@entralesupelec.fr>
2021-05-16 16:31:12 +0200

    updt

--- examples/demos/twins-paradox.py
+++ examples/demos/twins-paradox.py
@@ -2,19 +2,20 @@ import relativipy as rel
 import numpy as np
 
 
-universe = rel.scene.Universe(screen_size = (3, 16), width=800, height=450)
+universe = rel.scene.Universe(screen_size = (3, 10), width=800, height=450)
 universe.spacetime_mode   = False
 universe.show_prism_lines = False
-universe.t_max            = 10
+universe.t_max            = 6
+universe.axes_origin      = None
 
 human_size           = 1
 ground_level         = -human_size/2
-travel_speed         = universe.C*.75
-travel_go_duration   = 2
+travel_speed         = universe.C*.7
+travel_go_duration   = 1.5
 travel_back_duration = travel_go_duration
-chrono_period        = .25
+chrono_period        = .215
 
-universe.axes_origin = (-1, ground_level)
+universe += rel.objects.Prism(None, universe.C, (None, None ), np.array([(-5, ground_level), (5, ground_level)]), (0, .75, 0))
 
 head  = np.array([[np.sin(t), -np.cos(t)] for t in np.linspace(0, 2*np.pi, 11)])
 left  = np.array([(-1, 0), (-1, 3), (-1, 2), (-2, 2), (-2, 4)])
@@ -24,7 +25,7 @@ human = np.vstack((left, head + np.array([0, 5]), right, np.array([left[0]]))) *
 
 ship = np.array([(-5, -4), (-3, -1), (-3, 3), (-2, 4), (0, 5), (2, 4), (3, 3), (3, -1), (5, -4), (-5, -4)]) * np.array([.15, .2])
 universe += rel.objects.Prism((0, travel_speed), universe.C, (None, None), ship, (.2, .2, .2))
-ship = ship * np.array([1, -1]) + np.array([0, 7])
+ship = ship * np.array([1, -1]) + np.array([0, 4])
 universe += rel.objects.Prism((0, - travel_speed), universe.C, (None, None), ship, (.2, .2, .2))
 
 twin_start_position = (0, ground_level)
@@ -53,15 +54,15 @@ universe += olivier_back
 
 start_xyt_R0 = start_evts.events[0] * np.array([1, 1, 1/universe.C])
 nb_ticks_A = 6
-nb_ticks_B = 13
-nb_ticks_C = 5
+nb_ticks_B = 9
+nb_ticks_C = 4
 epsilon = 1e-3
 start_evt = start_xyt_R0
 end_time   = start_evt[2] + nb_ticks_A * chrono_period
 chrono_herve_A = rel.objects.Chronometer(None, universe.C, start_evt, chrono_period, end_time - start_evt[2] - epsilon, (1, .7, .7))
 start_evt[2] = end_time
 end_time += nb_ticks_B * chrono_period
-chrono_herve_B = rel.objects.Chronometer(None, universe.C, start_evt, chrono_period, end_time - start_evt[2] - epsilon, (.5, .2, .2))
+chrono_herve_B = rel.objects.Chronometer(None, universe.C, start_evt, chrono_period, end_time - start_evt[2] - epsilon, (0, 1, 1))
 start_evt[2] = end_time
 end_time += nb_ticks_C * chrono_period
 chrono_herve_C = rel.objects.Chronometer(None, universe.C, start_evt, chrono_period, end_time - start_evt[2] + epsilon, (1, .7, .7))
@@ -147,7 +148,7 @@ print()
 print()
 nb_ticks_herve = len(chrono_herve_A.events) + len(chrono_herve_B.events) + len(chrono_herve_C.events)
 nb_ticks_olivier = len(chrono_olivier_go.events) + len(chrono_olivier_back.events) - 1
-text = 'Hervé, staying on the ground, has experienced {} proper chronometer\nticks while Olivier, travelling, has experienced only {} of them. At the\nend, Hervé is {} seconds older than Olivier. Olivier has juped {} ticks\nin the future of Hervé at U-turn.'
+text = 'Hervé, staying on the ground, has experienced {} proper chronometer\nticks while Olivier, travelling, has experienced only {} of them. At the\nend, Hervé is {} seconds older than Olivier. Olivier has jumped {} ticks\nin the future of Hervé at U-turn.'
 print(text.format(nb_ticks_herve, nb_ticks_olivier, (nb_ticks_herve - nb_ticks_olivier) * chrono_period,  len(chrono_herve_B.events)))
 print()
 print()
