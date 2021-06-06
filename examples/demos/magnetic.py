3a059c94440140430b59ac4856d27bbd629abcc2
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2021-06-06 17:46:50 +0200

    updt

--- examples/demos/magnetic.py
+++ examples/demos/magnetic.py
@@ -1,7 +1,7 @@
 import relativipy as rel
 import numpy as np
 
-universe = rel.scene.Universe(screen_size = (4, 2), width=800, height=450)
+universe = rel.universe.Relativist(screen_size = (4, 2), width=800, height=450)
 
 Xampl = 10
 Xnbs  = 61
@@ -10,11 +10,11 @@ dy = .05
 
 current_speed = (universe.C*.75, 0)
 atoms = np.array([[x, dy] for x in Xs])
-atoms = rel.objects.Points(None, universe.C, (None, None), atoms, (1, 0, 0))
+atoms = rel.objects.Points(universe, None, (None, None), atoms, (1, 0, 0))
 
 dilatation_coef = 1.6 # Ok... I have tweaked it empirically.
 electrons = np.array([[x, -dy] for x in Xs*dilatation_coef])
-electrons = rel.objects.Points(current_speed, universe.C, (None, None), electrons, (0, .75, 0))
+electrons = rel.objects.Points(universe, current_speed, (None, None), electrons, (0, .75, 0))
 
 electrons.cross_radius = .02
 atoms.cross_radius     = .02
